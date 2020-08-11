d0e862864c95deb9217ab09997d51286fd19339a
Samir Sayyad <samir.saiad@gmail.com>
2020-08-11 14:19:16 +0430

    replace with emb

--- static/js/linkL10n.js
+++
@@ -1,8 +0,0 @@
-/* ***** Public methods: ***** */
-
-var localize = function(element) {
-    html10n.translateElement(html10n.translations, element.get(0));
-  };
-  
-  exports.localize = localize;
-  
---
+++ static/js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+  $("#insertEmbedMedia").click(function () {
+    // Can not use this yet, fix in main etherpad
+    // padeditbar.toogleDropDown("embedMediaModal");
+    var module = $("#embedMediaModal");
+    console.log(module)
+    if (!module.hasClass('insertEmbedMedia')) {
+      module.addClass("insertEmbedMedia-show");
+    } else {
+      module.removeClass("insertEmbedMedia-show");
+    }
+  });
+
+  $("#doEmbedMedia").click(function () {
+    var padeditor = require('ep_etherpad-lite/static/js/pad_editor').padeditor;
+
+    $("#embedMediaModal").slideUp("fast");
+
+    return padeditor.ace.callWithAce(function (ace) {
+      var rep = ace.ace_getRep();
+      ace.ace_replaceRange(rep.selStart, rep.selEnd, "E");
+      ace.ace_performSelectionChange([rep.selStart[0],rep.selStart[1]-1], rep.selStart, false);
+      ace.ace_performDocumentApplyAttributesToRange(rep.selStart, rep.selEnd, [["embedMedia", escape($("#embedMediaSrc")[0].value)]]);
+    }, "embedMedia");
+  });
+
+  $("#cancelEmbedMedia").click(function () {
+    $("#embedMediaModal").slideUp("fast");
+  });
+});
