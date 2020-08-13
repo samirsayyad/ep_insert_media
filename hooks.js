187d1cc10e5c316fe6d4f2db52dcce62d9a71fc3
Samir Sayyad <samir.saiad@gmail.com>
2020-08-13 15:14:43 +0430

    starting to have upload file

--- hooks.js
+++ hooks.js
@@ -1,7 +1,9 @@
 var path = require('path'), 
     express = require('ep_etherpad-lite/node_modules/express'),
     eejs = require("ep_etherpad-lite/node/eejs");
-
+    Busboy = require('busboy');
+    StreamUpload = require('stream_upload');
+    
 exports.eejsBlock_editbarMenuLeft = function (hook_name, args, cb) {
   args.content = args.content + eejs.require("ep_insert_media/templates/editbarButtons.ejs", {}, module);
   return cb();
@@ -20,4 +22,11 @@ exports.eejsBlock_scripts = function (hook_name, args, cb) {
 exports.eejsBlock_styles = function (hook_name, args, cb) {
   args.content = args.content + eejs.require("ep_insert_media/templates/styles.ejs", {}, module);
   return cb();
+}
+
+exports.expressConfigure = function (hookName, context) {
+  context.app.post('/p/:padId/pluginfw/ep_insert_media/upload', function (req, res, next) {
+    console.log('expressConfigure POST PARAMS', req.params);
+
+  })
 }
--- static/js/main.js
+++ static/js/main.js
@@ -41,4 +41,33 @@ $(document).ready(function () {
     $("#embedMediaModal").removeClass("insertEmbedMedia-show");
   });
 
+
+
+
+  $("#but_upload").click(function(){
+
+    var fd = new FormData();
+    var files = $('#file')[0].files[0];
+    fd.append('file',files);
+
+    $.ajax({
+        url: '/p/' + clientVars.padId + '/pluginfw/ep_insert_media/upload',
+        type: 'post',
+        data: fd,
+        contentType: false,
+        processData: false,
+        success: function(response){
+            if(response != 0){
+                $("#img").attr("src",response); 
+                $(".preview img").show(); // Display image element
+            }else{
+                alert('file not uploaded');
+            }
+        },
+    });
+  });
+
+
+
+
 });
